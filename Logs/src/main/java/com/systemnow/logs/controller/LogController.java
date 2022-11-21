68752812c466ef9a3da7633c9cd2ebc9af952177
Jailson Alves <jailson.alves@timwetech.com>
2022-11-21 17:14:34 +0000

    add swagger configuration

--- Logs/src/main/java/com/systemnow/logs/controller/LogController.java
+++ Logs/src/main/java/com/systemnow/logs/controller/LogController.java
@@ -4,6 +4,9 @@ import com.systemnow.logs.controller.response.LoadResponse;
 import com.systemnow.logs.converter.LoadConvert;
 import com.systemnow.logs.model.Load;
 import com.systemnow.logs.service.LoadService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -29,6 +32,10 @@ public class LogController {
      * @param groupName
      * @return name of created Load
      */
+    @ApiOperation(value = "Get file logs from pods, Param: Group name ")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK", response = String.class),
+            @ApiResponse(code = 500, message = "Internal server error")})
     @PostMapping("/getFiles")
     public ResponseEntity<String> getFiles(@RequestParam(required = false) String groupName) {
         log.info("getFiles START");
@@ -46,23 +53,27 @@ public class LogController {
     }
 
     /**
-     * Read file from folder name and save to database.
-     * @param name
-     * @return name of load
+     * Read file from folder loadName and save to database.
+     * @param loadName
+     * @return loadName of load
      */
+    @ApiOperation(value = "Read file from folder loadName and save to database, param: Load name")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK", response = LoadResponse.class),
+            @ApiResponse(code = 500, message = "Internal server error")})
     @PostMapping("/readFiles")
-    public ResponseEntity<String> doReadFiles(@RequestParam String name) {
+    public ResponseEntity<String> doReadFiles(@RequestParam String loadName) {
         log.info("doReadFiles START");
 
         try {
-            loadService.justReadFiles(name);
+            loadService.justReadFiles(loadName);
         } catch (Exception e) {
             log.error(e.getMessage());
-            return new ResponseEntity<>("ERROR, load: "+name, null, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("ERROR, load: "+loadName, null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
         log.info("doReadFiles END");
 
-        return new ResponseEntity<>("load: "+name +" executing!", null, HttpStatus.OK);
+        return new ResponseEntity<>("load: "+loadName +" executing!", null, HttpStatus.OK);
     }
 
     /**
@@ -70,6 +81,10 @@ public class LogController {
      * @param groupName
      * @return name of created Load
      */
+    @ApiOperation(value = "Do the complete load (get files, read files and save content to database), param: Group name")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK", response = LoadResponse.class),
+            @ApiResponse(code = 500, message = "Internal server error")})
     @PostMapping("/load")
     public ResponseEntity<String> completeLoad(@RequestParam(required = false) String groupName) {
         log.info("Load START");
@@ -87,6 +102,10 @@ public class LogController {
         return new ResponseEntity<>("load : "+loadName +" executing!", null, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "get loads by filter Load name")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK", response = LoadResponse.class),
+            @ApiResponse(code = 404, message = "Not found")})
     @GetMapping("/load/{name}")
     public ResponseEntity<LoadResponse> getLoad(@PathVariable String name) {
         log.info("getLoad by loadName: {}", name);
@@ -100,6 +119,10 @@ public class LogController {
         return new ResponseEntity<>(loadResponse, null, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "get loads by filter page and size")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK", response = Page.class),
+            @ApiResponse(code = 404, message = "Not found")})
     @GetMapping("/load")
     public ResponseEntity<Page<LoadResponse>> getAllLoad(@RequestParam(required = false, defaultValue = "0") int page,
                                                          @RequestParam(required = false, defaultValue = "10") int size) {
